3c10f9b13efce4bd2fc146e2895dfe4e282a32ad
Manogna007 <saimanogna49@gmail.com>
2024-11-05 09:50:37 +0530

    Counter functionality implemented

---
+++ javascript.js
@@ -0,0 +1,17 @@
+ 
+        let count = 0;
+
+        function updateCounter() {
+            document.getElementById("counter").textContent = count;
+        }
+
+        function increase() {
+            count++;
+            updateCounter();
+        }
+
+        function decrease() {
+            count--;
+            updateCounter();
+        }
+  
